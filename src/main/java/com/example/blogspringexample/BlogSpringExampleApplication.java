4a16f4b4e0519c8dc305d7e2ad1d5a9df445d1ae
toughchb <ggang2000@gmail.com>
2024-02-12 21:10:15 +0900

    project init

---
+++ src/main/java/com/example/blogspringexample/BlogSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blogspringexample/replacement/StringReplacementTest.java
@@ -0,0 +1,5 @@
+package com.example.blogspringexample.replacement;
+
+public class StringReplacementTest {
+
+}
